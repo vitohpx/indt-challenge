0f4a6ffb388e9fe22e376fc85ae50167f60eb68f
vitohpx <vhpx@icomp.ufam.edu.br>
2023-12-28 13:12:36 -0400

    first commit

---
+++ backend/UserApi/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+   
+    }
+}
---
+++ backend/UserApi/DTO/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace UserApi.DTO
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
