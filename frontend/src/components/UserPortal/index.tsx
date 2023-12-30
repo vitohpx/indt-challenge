9e44a070e4a422bda13f318a2e1b8ad1cc65c09a
vitohpx <vhpx@icomp.ufam.edu.br>
2023-12-30 16:33:54 -0400

    add create user function to user portal

--- frontend/src/components/UserPortal/index.tsx
+++ frontend/src/components/UserPortal/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Button, Form, Alert  } from 'react-bootstrap';
-import { getUserList, deleteUser, updateUser } from '../../services/user.service';
+import { Table, Button, Form, Alert, Modal} from 'react-bootstrap';
+import { getUserList, addUser, deleteUser, updateUser } from '../../services/user.service';
 
 enum UserType {
   Admin = 0,
@@ -19,8 +19,18 @@ interface User {
 const UserPortal: React.FC = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [editUser, setEditUser] = useState<User | null>(null);
-    const [isEditing, setIsEditing] = useState(false);
-    const [showAlert, setShowAlert] = useState(false);
+    const [showEditAlert, setEditShowAlert] = useState(false);
+    const [showCreateAlert, setCreateShowAlert] = useState(false);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [showDeleteAlert, setDeleteShowAlert] = useState(false);
+    const [newUser, setNewUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        userType: UserType.Common,
+    });
+    const [editingUserId, setEditingUserId] = useState<number | null>(null);
 
     const userTypeToString = (userType: UserType): string => {
         switch (userType) {
@@ -46,17 +56,20 @@ const UserPortal: React.FC = () => {
     const handleDelete = async (id: number) => {
         await deleteUser(id);
         fetchUsers();
-       
+        setDeleteShowAlert(true);
+        setTimeout(() => {
+            setDeleteShowAlert(false);
+        }, 3000);
     };
 
     const handleEdit = (user: User) => {
         setEditUser({ ...user });
-        setIsEditing(true);
+        setEditingUserId(user.id);
     };
 
     const handleCancelEdit = () => {
         setEditUser(null);
-        setIsEditing(false);
+        setEditingUserId(null);
     };
     
 
@@ -70,12 +83,12 @@ const UserPortal: React.FC = () => {
             userType: updatedUser.userType
             };
             await updateUser(id, updatedFields);
-            setShowAlert(true);
+            setEditShowAlert(true);
             setTimeout(() => {
-            setShowAlert(false);
+            setEditShowAlert(false);
             }, 3000);
             setEditUser(null);
-            setIsEditing(false);
+            setEditingUserId(null);
             fetchUsers();
         }
     };      
@@ -91,15 +104,113 @@ const UserPortal: React.FC = () => {
             setEditUser({ ...editUser, userType: Number(e.target.value) as UserType });
         }
     };    
+
+    const handleCreateUser = async () => {
+        try {
+          await addUser(newUser);
+          setCreateShowAlert(true);
+          setTimeout(() => {
+            setCreateShowAlert(false);
+          }, 5000);
+          setShowCreateModal(false);
+          setNewUser({
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            userType: UserType.Common,
+          });
+          fetchUsers();
+        } catch (error) {
+          console.error('Erro ao criar usuário:', error);
+        }
+    };
     
     return (
     <div>
-      <h2>User List</h2>
-      {showAlert && (
-        <Alert variant="success" onClose={() => setShowAlert(false)} dismissible>
+      <h2>User Portal</h2>
+      {showEditAlert && (
+        <Alert variant="success" onClose={() => setEditShowAlert(false)} dismissible>
           Usuário editado com sucesso!
         </Alert>
       )}
+      {showCreateAlert && (
+        <Alert variant="success" onClose={() => setCreateShowAlert(false)} dismissible>
+          Usuário criado com sucesso!
+        </Alert>
+      )}
+      {showDeleteAlert && (
+        <Alert variant="success" onClose={() => setDeleteShowAlert(false)} dismissible>
+          Usuário removido com sucesso!
+        </Alert>
+      )}
+      <Button variant="primary" onClick={() => setShowCreateModal(true)}>
+        Create User
+      </Button>
+      <Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter first name"
+                value={newUser.firstName}
+                onChange={(e) => setNewUser({ ...newUser, firstName: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter last name"
+                value={newUser.lastName}
+                onChange={(e) => setNewUser({ ...newUser, lastName: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={newUser.email}
+                onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={newUser.password}
+                onChange={(e) => setNewUser({ ...newUser, password: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formUserType">
+              <Form.Label>User Type</Form.Label>
+              <Form.Control
+                as="select"
+                value={newUser.userType}
+                onChange={(e) => setNewUser({ ...newUser, userType: Number(e.target.value) })}
+              >
+                <option value={UserType.Admin}>{userTypeToString(UserType.Admin)}</option>
+                <option value={UserType.Common}>{userTypeToString(UserType.Common)}</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowCreateModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCreateUser}>
+            Create User
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Table bordered>
         <thead>
           <tr>
@@ -116,11 +227,11 @@ const UserPortal: React.FC = () => {
                 <tr key={user.id}>
                     <td>{user.id}</td>
                     <td>
-                    {isEditing && editUser?.id === user.id ? (
+                    {editingUserId === user.id ? (
                         <Form.Control
                         type="text"
                         name="firstName"
-                        value={editUser.firstName}
+                        value={editUser?.firstName}
                         onChange={handleChange}
                         />
                     ) : (
@@ -128,11 +239,11 @@ const UserPortal: React.FC = () => {
                     )}
                     </td>
                     <td>
-                        {editUser && editUser.id === user.id ? (
+                        {editingUserId === user.id ? (
                         <Form.Control
                             type="text"
                             name="lastName"
-                            value={editUser.lastName}
+                            value={editUser?.lastName}
                             onChange={handleChange}
                         />
                         ) : (
@@ -140,11 +251,11 @@ const UserPortal: React.FC = () => {
                         )}
                     </td>
                     <td>
-                        {editUser && editUser.id === user.id ? (
+                        {editingUserId === user.id ? (
                         <Form.Control
                             type="text"
                             name="email"
-                            value={editUser.email}
+                            value={editUser?.email}
                             onChange={handleChange}
                         />
                         ) : (
@@ -162,7 +273,7 @@ const UserPortal: React.FC = () => {
                             )}
                     </td>
                     <td>
-                        {isEditing ? (
+                        {editingUserId === user.id ? (
                         <>
                             <Button variant="success" onClick={() => handleUpdate(user.id, editUser)}>
                             Update
